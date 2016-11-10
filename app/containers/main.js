7784ac33230888c456188dc1f35da886cbaa2366
Ruben Sanz <info@rubensanz.es>
2016-11-10 21:03:51 +0100

    chore: add redux and router

---
+++ app/containers/main.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ToastAndroid,
+  Alert,
+  AsyncStorage,
+} from 'react-native';
+import { Router, Scene, Actions, ActionConst } from 'react-native-router-flux';
+import { connect, Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
+const RouterWithRedux = connect()(Router);
+import reducers from '../reducers';
+
+import LogInComponent from '../components/Login.js';
+import MainSceneComponent from '../components/MainScene.js';
+
+const scenes = Actions.create(
+    <Scene key="root">
+      <Scene key="main" component={MainSceneComponent} type={ActionConst.REPLACE} title="Main"/>
+      <Scene key="login" component={LogInComponent} title="Log in" initial={true}/>
+    </Scene>
+);
+
+const middleware = [/* ...your middleware (i.e. thunk) */];
+const store = compose(
+  applyMiddleware(...middleware)
+)(createStore)(reducers);
+
+export default class lightMap extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      tokenId: ''
+    };
+  }
+
+  render() {
+
+    AsyncStorage.getItem("tokenId").then((value) => {
+      if (value && value !== '') {
+        this.setState({"tokenId": value});
+        Actions.main();
+      }
+      else {
+        Alert.alert('You do not have tokenId!');
+      }
+    }).catch((error) => {
+      Alert.alert('Oooops!');
+    });
+
+    // ToastAndroid.show('This is a toast with short duration', ToastAndroid.LONG);
+
+    // return (
+    //   <View style={styles.container}>
+    //     <Text
+    //       style={styles.welcome}
+    //       onPress={() => ToastAndroid.show('Ok, great, you pressed a text... lucky you!', ToastAndroid.LONG)} >
+    //       Hello world! Let's login
+    //     </Text>
+    //   </View>
+    // );
+
+    return (
+      <Provider store={store}>
+        <RouterWithRedux scenes={scenes}>
+        </RouterWithRedux>
+      </Provider>
+    );
+
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+// });
