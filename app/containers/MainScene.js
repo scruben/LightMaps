667f81ed85ff4088e8ad7703933a9bb67308c680
Ruben Sanz <info@rubensanz.es>
2016-11-14 16:21:00 +0100

    chore: cleaning code

--- app/containers/MainScene.js
+++ app/containers/MainScene.js
@@ -188,7 +188,7 @@ class MainSceneComponent extends Component {
   };
 
   render() {
-    StatusBar.setHidden(true);
+    StatusBar.setHidden(false);
     return (
       <View style={styles.container}>
         <MapView
@@ -197,11 +197,12 @@ class MainSceneComponent extends Component {
           initialCenterCoordinate={this.state.center}
           initialZoomLevel={this.state.zoom}
           initialDirection={0}
-          rotateEnabled={true}
+          rotateEnabled={false}
           scrollEnabled={true}
           zoomEnabled={true}
-          showsUserLocation={false}
-          styleURL={Mapbox.mapStyles.dark}
+          showsUserLocation={true}
+          loadingEnabled={true}
+          styleURL={Mapbox.mapStyles.streets}
           userTrackingMode={this.state.userTrackingMode}
           annotations={this.state.annotations}
           annotationsAreImmutable
@@ -214,9 +215,7 @@ class MainSceneComponent extends Component {
           onLongPress={this.onLongPress}
           onTap={this.onTap}
         />
-      <ScrollView style={styles.scrollView}>
         {this._renderButtons()}
-      </ScrollView>
       </View>
     );
   }
@@ -224,9 +223,6 @@ class MainSceneComponent extends Component {
   _renderButtons() {
     return (
       <View>
-        <Text onPress={() => this._map && this._map.setDirection(0)}>
-          Set direction to 0
-        </Text>
         <Text onPress={() => this._map && this._map.setZoomLevel(6)}>
           Zoom out to zoom level 6
         </Text>
@@ -236,92 +232,14 @@ class MainSceneComponent extends Component {
         <Text onPress={() => this._map && this._map.setCenterCoordinateZoomLevel(35.68829, 139.77492, 14)}>
           Go to Tokyo at fixed zoom level 14
         </Text>
-        <Text onPress={() => this._map && this._map.easeTo({ pitch: 30 })}>
-          Set pitch to 30 degrees
-        </Text>
         <Text onPress={this.addNewMarkers}>
           Add new marker
-        </Text>
-        <Text onPress={this.updateMarker2}>
-          Update marker2
-        </Text>
-        <Text onPress={() => this._map && this._map.selectAnnotation('marker1')}>
-          Open marker1 popup
-        </Text>
-        <Text onPress={() => this._map && this._map.deselectAnnotation()}>
-          Deselect annotation
-        </Text>
-        <Text onPress={this.removeMarker2}>
-          Remove marker2 annotation
-        </Text>
-        <Text onPress={() => this.setState({ annotations: [] })}>
-          Remove all annotations
-        </Text>
-        <Text onPress={() => this._map && this._map.setVisibleCoordinateBounds(40.712, -74.227, 40.774, -74.125, 100, 0, 0, 0)}>
-          Set visible bounds to 40.7, -74.2, 40.7, -74.1
-        </Text>
-        <Text onPress={() => this.setState({ userTrackingMode: Mapbox.userTrackingMode.followWithHeading })}>
-          Set userTrackingMode to followWithHeading
         </Text>
         <Text onPress={() => this._map && this._map.getCenterCoordinateZoomLevel((location)=> {
             console.log(location);
           })}>
           Get location
         </Text>
-        <Text onPress={() => this._map && this._map.getDirection((direction)=> {
-            console.log(direction);
-          })}>
-          Get direction
-        </Text>
-        <Text onPress={() => this._map && this._map.getBounds((bounds)=> {
-            console.log(bounds);
-          })}>
-          Get bounds
-        </Text>
-        <Text onPress={() => {
-            Mapbox.addOfflinePack({
-              name: 'test',
-              type: 'bbox',
-              bounds: [0, 0, 0, 0],
-              minZoomLevel: 0,
-              maxZoomLevel: 0,
-              metadata: { anyValue: 'you wish' },
-              styleURL: Mapbox.mapStyles.dark
-            }).then(() => {
-              console.log('Offline pack added');
-            }).catch(err => {
-              console.log(err);
-            });
-        }}>
-          Create offline pack
-        </Text>
-        <Text onPress={() => {
-            Mapbox.getOfflinePacks()
-              .then(packs => {
-                console.log(packs);
-              })
-              .catch(err => {
-                console.log(err);
-              });
-        }}>
-          Get offline packs
-        </Text>
-        <Text onPress={() => {
-            Mapbox.removeOfflinePack('test')
-              .then(info => {
-                if (info.deleted) {
-                  console.log('Deleted', info.deleted);
-                } else {
-                  console.log('No packs to delete');
-                }
-              })
-              .catch(err => {
-                console.log(err);
-              });
-        }}>
-          Remove pack with name 'test'
-        </Text>
-        <Text>User tracking mode is {this.state.userTrackingMode}</Text>
       </View>
     );
   }
@@ -334,10 +252,10 @@ const styles = StyleSheet.create({
     alignItems: 'stretch'
   },
   map: {
-    flex: 1
+    height: 1000
   },
   scrollView: {
-    height: 50
+    flex: 0
   }
 });
 
