5ae619456088c180c577570323e659bab984665f
Ruben Sanz <info@rubensanz.es>
2016-11-14 19:23:43 +0100

    feat: add zoom and current position buttons

--- app/containers/MainScene.js
+++ app/containers/MainScene.js
@@ -41,58 +41,37 @@ class MainSceneComponent extends Component {
       Actions.login();
     });
 
+    this.zoomPlus = zoomPlus.bind(this);
+    this.zoomMinus = zoomMinus.bind(this);
+    this.currentPos = currentPos.bind(this);
     // Load map data
 
   }
 
   state = {
     center: {
-      latitude: 40.72052634,
-      longitude: -73.97686958312988
+      latitude: 41.394501,
+      longitude: 2.187818
     },
+    currentPosition: {},
     zoom: 14,
     userTrackingMode: Mapbox.userTrackingMode.none,
     annotations: [{
-      coordinates: [42.72052634, -73.97686958312988],
+      coordinates: [41.394501,2.197818],
       type: 'point',
-      title: 'This is marker 1',
-      subtitle: 'It has a rightCalloutAccessory too',
+      title: 'Pingpongworks',
+      subtitle: 'Ping pong school',
       rightCalloutAccessory: {
-        source: { uri: 'https://cldup.com/9Lp0EaBw5s.png' },
+        source: { uri: 'http://pngimg.com/upload/small/ping_pong_PNG10381.png' },
         height: 25,
         width: 25
       },
       annotationImage: {
-        source: { uri: 'https://cldup.com/CnRLZem9k9.png' },
+        source: { uri: 'http://pngimg.com/upload/small/ping_pong_PNG10381.png' },
         height: 25,
         width: 25
       },
       id: 'marker1'
-    }, {
-      coordinates: [40.714541341726175,-74.00579452514648],
-      type: 'point',
-      title: 'Important!',
-      subtitle: 'Neat, this is a custom annotation image',
-      annotationImage: {
-        source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
-        height: 25,
-        width: 25
-      },
-      id: 'marker2'
-    }, {
-      coordinates: [[40.76572150042782,-73.99429321289062],[40.743485405490695, -74.00218963623047],[40.728266950429735,-74.00218963623047],[40.728266950429735,-73.99154663085938],[40.73633186448861,-73.98983001708984],[40.74465591168391,-73.98914337158203],[40.749337730454826,-73.9870834350586]],
-      type: 'polyline',
-      strokeColor: '#00FB00',
-      strokeWidth: 4,
-      strokeAlpha: .5,
-      id: 'foobar'
-    }, {
-      coordinates: [[40.749857912194386, -73.96820068359375], [40.741924698522055,-73.9735221862793], [40.735681504432264,-73.97523880004883], [40.7315190495212,-73.97438049316406], [40.729177554196376,-73.97180557250975], [40.72345355209305,-73.97438049316406], [40.719290332250544,-73.97455215454102], [40.71369559554873,-73.97729873657227], [40.71200407096382,-73.97850036621094], [40.71031250340588,-73.98691177368163], [40.71031250340588,-73.99154663085938]],
-      type: 'polygon',
-      fillAlpha: 1,
-      strokeColor: '#ffffff',
-      fillColor: '#0000ff',
-      id: 'zap'
     }]
   };
 
@@ -104,7 +83,9 @@ class MainSceneComponent extends Component {
     console.log('onRegionWillChange', location);
   };
   onUpdateUserLocation = (location) => {
-    console.log('onUpdateUserLocation', location);
+    // console.log('onUpdateUserLocation', location);
+    this.setState({ currentPosition: location });
+    console.log(this.state.currentPosition)
   };
   onOpenAnnotation = (annotation) => {
     console.log('onOpenAnnotation', annotation);
@@ -117,74 +98,6 @@ class MainSceneComponent extends Component {
   };
   onTap = (location) => {
     console.log('onTap', location);
-  };
-  onChangeUserTrackingMode = (userTrackingMode) => {
-    this.setState({ userTrackingMode });
-    console.log('onChangeUserTrackingMode', userTrackingMode);
-  };
-
-  componentWillMount() {
-    this._offlineProgressSubscription = Mapbox.addOfflinePackProgressListener(progress => {
-      console.log('offline pack progress', progress);
-    });
-    this._offlineMaxTilesSubscription = Mapbox.addOfflineMaxAllowedTilesListener(tiles => {
-      console.log('offline max allowed tiles', tiles);
-    });
-    this._offlineErrorSubscription = Mapbox.addOfflineErrorListener(error => {
-      console.log('offline error', error);
-    });
-  }
-
-  componentWillUnmount() {
-    this._offlineProgressSubscription.remove();
-    this._offlineMaxTilesSubscription.remove();
-    this._offlineErrorSubscription.remove();
-  }
-
-  addNewMarkers = () => {
-    // Treat annotations as immutable and create a new one instead of using .push()
-    this.setState({
-      annotations: [ ...this.state.annotations, {
-        coordinates: [40.73312,-73.989],
-        type: 'point',
-        title: 'This is a new marker',
-        id: 'foo'
-      }, {
-        'coordinates': [[40.749857912194386, -73.96820068359375], [40.741924698522055,-73.9735221862793], [40.735681504432264,-73.97523880004883], [40.7315190495212,-73.97438049316406], [40.729177554196376,-73.97180557250975], [40.72345355209305,-73.97438049316406], [40.719290332250544,-73.97455215454102], [40.71369559554873,-73.97729873657227], [40.71200407096382,-73.97850036621094], [40.71031250340588,-73.98691177368163], [40.71031250340588,-73.99154663085938]],
-        'type': 'polygon',
-        'fillAlpha': 1,
-        'fillColor': '#000000',
-        'strokeAlpha': 1,
-        'id': 'new-black-polygon'
-      }]
-    });
-  };
-
-  updateMarker2 = () => {
-    // Treat annotations as immutable and use .map() instead of changing the array
-    this.setState({
-      annotations: this.state.annotations.map(annotation => {
-        if (annotation.id !== 'marker2') { return annotation; }
-        return {
-          coordinates: [40.714541341726175,-74.00579452514648],
-          'type': 'point',
-          title: 'New Title!',
-          subtitle: 'New Subtitle',
-          annotationImage: {
-            source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
-            height: 25,
-            width: 25
-          },
-          id: 'marker2'
-        };
-      })
-    });
-  };
-
-  removeMarker2 = () => {
-    this.setState({
-      annotations: this.state.annotations.filter(a => a.id !== 'marker2')
-    });
   };
 
   render() {
@@ -202,7 +115,7 @@ class MainSceneComponent extends Component {
           zoomEnabled={true}
           showsUserLocation={true}
           loadingEnabled={true}
-          styleURL={Mapbox.mapStyles.streets}
+          styleURL={Mapbox.mapStyles.dark}
           userTrackingMode={this.state.userTrackingMode}
           annotations={this.state.annotations}
           annotationsAreImmutable
@@ -222,24 +135,26 @@ class MainSceneComponent extends Component {
 
   _renderButtons() {
     return (
-      <View>
-        <Text onPress={() => this._map && this._map.setZoomLevel(6)}>
-          Zoom out to zoom level 6
-        </Text>
-        <Text onPress={() => this._map && this._map.setCenterCoordinate(48.8589, 2.3447)}>
-          Go to Paris at current zoom level {parseInt(this.state.currentZoom)}
-        </Text>
-        <Text onPress={() => this._map && this._map.setCenterCoordinateZoomLevel(35.68829, 139.77492, 14)}>
-          Go to Tokyo at fixed zoom level 14
-        </Text>
-        <Text onPress={this.addNewMarkers}>
-          Add new marker
-        </Text>
-        <Text onPress={() => this._map && this._map.getCenterCoordinateZoomLevel((location)=> {
-            console.log(location);
-          })}>
-          Get location
-        </Text>
+      <View style={styles.buttonBar}>
+        <View style = {{flexDirection: 'row'}}>
+          <Button
+            onPress={this.zoomPlus}
+            title="Zoom +"
+            color="#222222"
+          />
+          <Button
+            onPress={this.zoomMinus}
+            title="Zoom -"
+            color="#222222"
+          />
+        </View>
+        <View style ={{marginLeft: 10}}>
+          <Button
+            onPress={this.currentPos}
+            title="My position"
+            color="#222222"
+          />
+        </View>
       </View>
     );
   }
@@ -252,12 +167,35 @@ const styles = StyleSheet.create({
     alignItems: 'stretch'
   },
   map: {
-    height: 1000
+    flex: 1,
   },
   scrollView: {
     flex: 0
+  },
+  buttonBar: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    padding: 10,
+    flexDirection: 'row'
   }
+
 });
+
+function zoomPlus () {
+  this._map.setZoomLevel(this.state.currentZoom+1, animated = true);
+}
+
+function zoomMinus () {
+  this._map.setZoomLevel(this.state.currentZoom-1, animated = true);
+}
+
+function currentPos () {
+  this._map.setCenterCoordinate(
+    this.state.currentPosition.latitude,
+    this.state.currentPosition.longitude,
+    animated = true);
+}
 
 const mapDispatchToProps = (dispatch) => ({
   setIdData: (token) => dispatch(setIdData(token)),
